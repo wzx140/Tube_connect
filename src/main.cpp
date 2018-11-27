17b426e16c3abb7d1411552b17357ff0f165285e
wzx <masterwangzx@gmail.com>
2018-11-27 23:38:47 +0800

    add test target
    Signed-off-by: wzx <masterwangzx@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <gtest/gtest.h>
+#include <vtkSmartPointer.h>
+#include "../include/CellPickerInteractorStyle.h"
+#include "../include/STLRender.h"
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    auto style = vtkSmartPointer<CellPickerInteractorStyle>::New();
+    auto stlRender = vtkSmartPointer<STLRender>::New();
+
+    style->SetDefaultRenderer(stlRender->getRenderer());
+    stlRender->setStyle(style);
+
+    stlRender->setPath("../test/res/test2.STL");
+    stlRender->load();
+    stlRender->setInputData(stlRender->getData());
+    style->setStlRender(stlRender);
+    style->setSelectMode(true);
+    stlRender->start();
 }
