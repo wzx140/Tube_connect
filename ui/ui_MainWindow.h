594de98c66ba296cbb6c5613c7c0bc672de4b7d0
wzx <masterwangzx@gmail.com>
2019-03-02 14:07:25 +0800

    fix ui

--- ui/ui_MainWindow.h
+++ ui/ui_MainWindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MainWindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.9.7
+** Created by: Qt User Interface Compiler version 5.12.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,9 +10,7 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
@@ -43,14 +41,14 @@ public:
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
-            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(1000, 600);
         centralwidget = new QWidget(MainWindow);
-        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         gridLayout = new QGridLayout(centralwidget);
-        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         openGLWidget = new QVTKOpenGLWidget(centralwidget);
-        openGLWidget->setObjectName(QStringLiteral("openGLWidget"));
+        openGLWidget->setObjectName(QString::fromUtf8("openGLWidget"));
         QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
@@ -61,15 +59,15 @@ public:
         gridLayout->addWidget(openGLWidget, 0, 0, 1, 1);
 
         verticalLayout = new QVBoxLayout();
-        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
         tableWidget = new QTableWidget(centralwidget);
         if (tableWidget->columnCount() < 1)
             tableWidget->setColumnCount(1);
         QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
-        __qtablewidgetitem->setText(QStringLiteral("Value"));
+        __qtablewidgetitem->setText(QString::fromUtf8("Value"));
         tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
-        if (tableWidget->rowCount() < 7)
-            tableWidget->setRowCount(7);
+        if (tableWidget->rowCount() < 6)
+            tableWidget->setRowCount(6);
         QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
         tableWidget->setVerticalHeaderItem(0, __qtablewidgetitem1);
         QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
@@ -83,29 +81,26 @@ public:
         QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
         tableWidget->setVerticalHeaderItem(5, __qtablewidgetitem6);
         QTableWidgetItem *__qtablewidgetitem7 = new QTableWidgetItem();
-        tableWidget->setVerticalHeaderItem(6, __qtablewidgetitem7);
+        __qtablewidgetitem7->setFlags(
+                Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsDragEnabled | Qt::ItemIsUserCheckable |
+                Qt::ItemIsEnabled);
+        tableWidget->setItem(0, 0, __qtablewidgetitem7);
         QTableWidgetItem *__qtablewidgetitem8 = new QTableWidgetItem();
         __qtablewidgetitem8->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(0, 0, __qtablewidgetitem8);
+        tableWidget->setItem(1, 0, __qtablewidgetitem8);
         QTableWidgetItem *__qtablewidgetitem9 = new QTableWidgetItem();
         __qtablewidgetitem9->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(1, 0, __qtablewidgetitem9);
+        tableWidget->setItem(2, 0, __qtablewidgetitem9);
         QTableWidgetItem *__qtablewidgetitem10 = new QTableWidgetItem();
         __qtablewidgetitem10->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(2, 0, __qtablewidgetitem10);
+        tableWidget->setItem(3, 0, __qtablewidgetitem10);
         QTableWidgetItem *__qtablewidgetitem11 = new QTableWidgetItem();
         __qtablewidgetitem11->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(3, 0, __qtablewidgetitem11);
+        tableWidget->setItem(4, 0, __qtablewidgetitem11);
         QTableWidgetItem *__qtablewidgetitem12 = new QTableWidgetItem();
         __qtablewidgetitem12->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(4, 0, __qtablewidgetitem12);
-        QTableWidgetItem *__qtablewidgetitem13 = new QTableWidgetItem();
-        __qtablewidgetitem13->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(5, 0, __qtablewidgetitem13);
-        QTableWidgetItem *__qtablewidgetitem14 = new QTableWidgetItem();
-        __qtablewidgetitem14->setFlags(Qt::ItemIsSelectable|Qt::ItemIsEditable|Qt::ItemIsDragEnabled|Qt::ItemIsUserCheckable|Qt::ItemIsEnabled);
-        tableWidget->setItem(6, 0, __qtablewidgetitem14);
-        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+        tableWidget->setItem(5, 0, __qtablewidgetitem12);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
         tableWidget->setEnabled(false);
         QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Preferred);
         sizePolicy1.setHorizontalStretch(0);
@@ -123,26 +118,26 @@ public:
         verticalLayout->addWidget(tableWidget);
 
         gridLayout_2 = new QGridLayout();
-        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
         importButton = new QPushButton(centralwidget);
-        importButton->setObjectName(QStringLiteral("importButton"));
+        importButton->setObjectName(QString::fromUtf8("importButton"));
 
         gridLayout_2->addWidget(importButton, 3, 0, 1, 1);
 
         runButton = new QPushButton(centralwidget);
-        runButton->setObjectName(QStringLiteral("runButton"));
+        runButton->setObjectName(QString::fromUtf8("runButton"));
         runButton->setEnabled(false);
 
         gridLayout_2->addWidget(runButton, 2, 0, 1, 1);
 
         exportButton = new QPushButton(centralwidget);
-        exportButton->setObjectName(QStringLiteral("exportButton"));
+        exportButton->setObjectName(QString::fromUtf8("exportButton"));
         exportButton->setEnabled(false);
 
         gridLayout_2->addWidget(exportButton, 3, 1, 1, 1);
 
         clearButton = new QPushButton(centralwidget);
-        clearButton->setObjectName(QStringLiteral("clearButton"));
+        clearButton->setObjectName(QString::fromUtf8("clearButton"));
         clearButton->setEnabled(false);
 
         gridLayout_2->addWidget(clearButton, 2, 1, 1, 1);
@@ -155,7 +150,7 @@ public:
 
         MainWindow->setCentralWidget(centralwidget);
         statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName(QStringLiteral("statusbar"));
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
@@ -169,39 +164,38 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Tube Connection", Q_NULLPTR));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Tube Connection", nullptr));
         QTableWidgetItem *___qtablewidgetitem = tableWidget->verticalHeaderItem(0);
-        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "file", Q_NULLPTR));
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "file", nullptr));
 #ifndef QT_NO_TOOLTIP
-        ___qtablewidgetitem->setToolTip(QApplication::translate("MainWindow", "path of file", Q_NULLPTR));
+        ___qtablewidgetitem->setToolTip(QApplication::translate("MainWindow", "path of file", nullptr));
 #endif // QT_NO_TOOLTIP
         QTableWidgetItem *___qtablewidgetitem1 = tableWidget->verticalHeaderItem(1);
-        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "length", Q_NULLPTR));
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "length", nullptr));
 #ifndef QT_NO_TOOLTIP
-        ___qtablewidgetitem1->setToolTip(QApplication::translate("MainWindow", "the length of each tube after connection", Q_NULLPTR));
+        ___qtablewidgetitem1->setToolTip(
+                QApplication::translate("MainWindow", "the length of each tube after connection", nullptr));
 #endif // QT_NO_TOOLTIP
         QTableWidgetItem *___qtablewidgetitem2 = tableWidget->verticalHeaderItem(2);
-        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "radius", Q_NULLPTR));
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "radius", nullptr));
 #ifndef QT_NO_TOOLTIP
-        ___qtablewidgetitem2->setToolTip(QApplication::translate("MainWindow", "radius of each tube", Q_NULLPTR));
+        ___qtablewidgetitem2->setToolTip(QApplication::translate("MainWindow", "radius of each tube", nullptr));
 #endif // QT_NO_TOOLTIP
         QTableWidgetItem *___qtablewidgetitem3 = tableWidget->verticalHeaderItem(3);
-        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "coefficient1", Q_NULLPTR));
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "coefficient1", nullptr));
         QTableWidgetItem *___qtablewidgetitem4 = tableWidget->verticalHeaderItem(4);
-        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "coefficient2", Q_NULLPTR));
+        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "coefficient2", nullptr));
         QTableWidgetItem *___qtablewidgetitem5 = tableWidget->verticalHeaderItem(5);
-        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "coefficient3", Q_NULLPTR));
-        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->verticalHeaderItem(6);
-        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "coefficient4", Q_NULLPTR));
+        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "coefficient3", nullptr));
 
         const bool __sortingEnabled = tableWidget->isSortingEnabled();
         tableWidget->setSortingEnabled(false);
         tableWidget->setSortingEnabled(__sortingEnabled);
 
-        importButton->setText(QApplication::translate("MainWindow", "IMPORT", Q_NULLPTR));
-        runButton->setText(QApplication::translate("MainWindow", "RUN", Q_NULLPTR));
-        exportButton->setText(QApplication::translate("MainWindow", "EXPORT", Q_NULLPTR));
-        clearButton->setText(QApplication::translate("MainWindow", "CLEAR", Q_NULLPTR));
+        importButton->setText(QApplication::translate("MainWindow", "IMPORT", nullptr));
+        runButton->setText(QApplication::translate("MainWindow", "RUN", nullptr));
+        exportButton->setText(QApplication::translate("MainWindow", "EXPORT", nullptr));
+        clearButton->setText(QApplication::translate("MainWindow", "CLEAR", nullptr));
     } // retranslateUi
 
 };
